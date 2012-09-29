06a4dce0185d230818615296d08857d3321924c7
yoomi pyo <yoomipyo3@gmail.com>
2012-09-29 04:37:23 -0400

    Basic UI frame

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include_layout_template('layouts/header.php'); ?>
+    <div class="menu">
+        <a href="http://www.w3schools.com" target="content">
+            <div class="menu_block">Game</div> 
+        </a>
+        <a href="http://www.gatech.edu" target="content">
+            <div class="menu_block">Question</div>
+        </a>
+        <a href="http://www.intel.com" target="content">
+            <div class="menu_block">Scavanger Hunt</div>
+        </a>
+    </div>
+    <div class="frame">
+        <iframe id="content" width="800" height="480" scrolling="no" frameborder="1">
+        </iframe>
+    </div>
+
+<?php include_layout_template('layouts/footer.php'); ?>  
---
+++ layouts/footer.php
@@ -0,0 +1,5 @@
+     	<div style="clear:both"></div>
+        </div>
+        <div id="footer">Copyright <?php echo date("Y", time()); ?>, ???</div>
+    </body>
+</html>
