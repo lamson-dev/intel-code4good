e98ab4d46dc5109b3befc1a3b293482863080bfd
yoomi pyo <yoomipyo3@gmail.com>
2012-09-29 09:23:23 -0400

    Added frame page

--- index.php
+++ index.php
@@ -11,16 +11,8 @@
         </a>
     </div>
     <div class="frame">
-        <iframe id="content" width="800" height="480" scrolling="no" frameborder="1">
+        <iframe id="content" src="main_frame.html" width="800" height="480" scrolling="no" frameborder="1">
         </iframe>
     </div>
-    	<div id="audio">
-			<audio controls="controls" autoplay loop>
-			  <source src="audios/chocobo.ogg" type="audio/ogg">
-			  <source src="audios/chocobo.mp3" type="audio/mpeg">
-			Your browser does not support the audio element.
-			</audio>
-		</div>
-
 
 <?php include_layout_template('layouts/footer.php'); ?>  
