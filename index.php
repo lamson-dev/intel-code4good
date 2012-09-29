174a228f198711eae698504e67e29daaf8d13b3f
Son Nguyen <lamson6592@gmail.com>
2012-09-29 06:41:53 -0400

    added audio to index page

--- index.php
+++ index.php
@@ -14,5 +14,13 @@
         <iframe id="content" width="800" height="480" scrolling="no" frameborder="1">
         </iframe>
     </div>
+    	<div id="audio">
+			<audio controls="controls" autoplay loop>
+			  <source src="audios/chocobo.ogg" type="audio/ogg">
+			  <source src="audios/chocobo.mp3" type="audio/mpeg">
+			Your browser does not support the audio element.
+			</audio>
+		</div>
+
 
 <?php include_layout_template('layouts/footer.php'); ?>  
